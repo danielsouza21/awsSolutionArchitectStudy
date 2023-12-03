734c17abe08f8b94e72247bb72a3659e6502d55b
Daniel <danielloko1999@hotmail.com>
2023-12-03 14:34:18 -0300

    Remove ignored files

--- AWS_Project_Study/Models/Configurations/S3BucketConfigs.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AWS_Project_Study.Models.Configurations
-{
-    public class S3BucketConfigs
-    {
-        public List<string> AllowedFiles { get; set; }
-        public string BucketName { get; set; }
-    }
-}
